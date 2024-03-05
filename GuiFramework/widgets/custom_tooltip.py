d8b205a68877fd1c718af6fb62b1ce5d33d46397
Jan Görl <JanPanthera@outlook.de>
2024-03-06 00:18:00 +0100

    Added custom tooltip and enhanced tree structure classes
    Added a new `custom_tooltip.py` file that includes a `CustomTooltip` class for creating tooltips for widgets. Enhanced the `Node`, `FileNode`, `FolderNode`, and `TreeStructure` classes in `tree_structure.py`. The `Node` class now includes several new attributes and an `update_icon` method. The `FileNode` class now includes methods for toggling, selecting, and deselecting nodes. The `FolderNode` class now includes new attributes for managing subnodes and their icons. The `TreeStructure` class now includes methods for expanding, collapsing, selecting, deselecting nodes, and getting selected files.

---
+++ GuiFramework/widgets/custom_tooltip.py
@@ -0,0 +1,64 @@
+import customtkinter as ctk
+
+class CustomTooltip:
+    def __init__(self, widget, text, delay=200):
+        self.widget = widget
+        self.text = text
+        self.tooltip_window = None
+        self.mouse_on_widget = False
+        self.mouse_on_tooltip = False
+        self.delay = delay
+        self.show_id = None
+        self.hide_id = None
+
+        self.widget.bind("<Enter>", self.schedule_show_tooltip)
+        self.widget.bind("<Leave>", self.schedule_hide_tooltip)
+
+    def schedule_show_tooltip(self, event=None):
+        self.cancel_scheduled_events()
+        self.mouse_on_widget = True
+        self.show_id = self.widget.after(self.delay, self.show_tooltip, event)
+
+    def show_tooltip(self, event=None):
+        if self.tooltip_window is not None:
+            return
+
+        x = event.x_root + 20
+        y = event.y_root + 10
+
+        self.tooltip_window = ctk.CTkToplevel(self.widget)
+        self.tooltip_window.wm_overrideredirect(True)
+        
+        label = ctk.CTkLabel(self.tooltip_window, text=self.text, corner_radius=10)
+        label.pack(fill="none", expand=False)
+
+        self.tooltip_window.update_idletasks()
+        width = label.winfo_reqwidth()
+        height = label.winfo_reqheight()
+
+        self.tooltip_window.wm_geometry(f"{width}x{height}+{x}+{y}")
+        
+        self.tooltip_window.bind("<Enter>", self.enter_tooltip)
+        self.tooltip_window.bind("<Leave>", self.schedule_hide_tooltip)
+
+    def enter_tooltip(self, event=None):
+        self.mouse_on_tooltip = True
+
+    def schedule_hide_tooltip(self, event=None):
+        self.cancel_scheduled_events()
+        self.mouse_on_widget = False
+        self.mouse_on_tooltip = False
+        self.hide_id = self.widget.after(self.delay, self.hide_tooltip)
+
+    def hide_tooltip(self):
+        if self.tooltip_window:
+            self.tooltip_window.destroy()
+            self.tooltip_window = None
+
+    def cancel_scheduled_events(self):
+        if self.show_id:
+            self.widget.after_cancel(self.show_id)
+        if self.hide_id:
+            self.widget.after_cancel(self.hide_id)
+        self.show_id = None
+        self.hide_id = None
--- GuiFramework/widgets/tree_structure.py
+++ GuiFramework/widgets/tree_structure.py
@@ -1,50 +1,97 @@
 import os
 import customtkinter as ctk
 
-# TODO: icon optionally a image file
+
 class Node(ctk.CTkButton):
-    def __init__(self, parent, path, icon=None, level=1, *args, **kwargs):
-        self.base_container = ctk.CTkFrame(parent, fg_color="transparent")
-        self.base_container.pack(padx=0, pady=0, anchor='nw')
+    def __init__(self, parent, path, icon=None, level=0, *args, **kwargs):
+        self.node_base_container = ctk.CTkFrame(parent, fg_color="transparent")
+        self.node_base_container.pack(padx=0, pady=0, ipadx=0, ipady=0, anchor='nw')
+        self.visible = False
+        self.path = path
+        self.name = os.path.basename(path)
+        self.icon = None
 
-        if icon:
-            self.icon = ctk.CTkLabel(self.base_container, text=icon, fg_color="transparent", anchor='nw')
-            self.icon.pack(side='left', padx=(0, 5), pady=0, anchor='nw')
+        if not isinstance(icon, str):
+            icon = self._get_default_icon(path)
 
-        super().__init__(self.base_container, text=os.path.basename(path), anchor='nw', corner_radius=0, *args, **kwargs)
-        self.pack(side='left', padx=0, pady=0, anchor='nw')
+        self.icon = ctk.CTkLabel(self.node_base_container, text=icon, fg_color="transparent", anchor='nw')
+        self.icon.pack(side='left', padx=(0, 5), pady=0, ipadx=0, ipady=0)
+
+        super().__init__(self.node_base_container, text=os.path.basename(path), anchor='nw', corner_radius=0, *args, **kwargs)
+        self.pack(side='left', padx=0, pady=0, ipadx=0, ipady=0)
         if level == 0:
             self.show()
         self.level = level
 
     def show(self):
-        self.base_container.pack(padx=2, pady=2, anchor='nw')
+        self.node_base_container.pack(padx=0, pady=0, anchor='nw')
+        self.visible = True
 
     def hide(self):
-        self.base_container.pack_forget()
+        self.node_base_container.pack_forget()
+        self.visible = False
+
+    def update_icon(self, icon):
+        self.icon.configure(text=icon)
+
+    def _get_default_icon(self, path):
+        if path.endswith((".zip", ".7z", ".tar", ".gz", ".bz2", ".xz")):
+            return "📦"
+        elif path.endswith(".iso"):
+            return "💿"
+        elif os.path.isfile(path):
+            return "📄"
+        elif os.path.isdir(path):
+            return "📁"
+        elif os.path.islink(path):
+            return "🔗"
+        else:
+            return "📝"
 
 
 class FileNode(Node):
     def __init__(self, parent, file_path, level=1, *args, **kwargs):
-        super().__init__(parent, file_path, icon="📄", level=level, hover=False, fg_color="transparent", *args, **kwargs)
+        super().__init__(parent, file_path, level=level, hover=False, fg_color="transparent", *args, **kwargs)
+        self.configure(command=self.toggle_selection)
+        self.selected = False
+
+    def toggle_selection(self):
+        if self.selected:
+            self.deselect()
+        else:
+            self.select()
+
+    def select(self):
+        if not self.selected and self.visible:
+            self.selected = True
+            self.configure(fg_color="#4d4d4d")
+
+    def deselect(self):
+        if self.selected and self.visible:
+            self.selected = False
+            self.configure(fg_color="transparent")
 
 
 class FolderNode(Node):
     def __init__(self, parent, folder_path, level=1, *args, **kwargs):
-        self.base_container = ctk.CTkFrame(parent, fg_color="transparent")
-        self.base_container.pack(padx=0, pady=0, anchor='nw')
+        self.expanded_icon = "📂"
+        self.collapsed_icon = "📁"
 
-        self.subnodes_container = ctk.CTkFrame(self.base_container, fg_color="transparent")
-        super().__init__(self.base_container, folder_path, icon="📁", level=level, fg_color="transparent", *args, **kwargs)
+        self.folder_base_container = ctk.CTkFrame(parent, fg_color="transparent")
+        self.folder_base_container.pack(padx=0, pady=0, ipadx=0, ipady=0, anchor='nw')
+
+        self.subnodes_container = ctk.CTkFrame(self.folder_base_container, fg_color="transparent")
+
+        super().__init__(self.folder_base_container, folder_path, level=level, fg_color="transparent", *args, **kwargs)
         self.configure(command=self.toggle_subnodes)
+
         self.subnodes = []
         self.expanded = False
-        self.folder_path = str(folder_path)
         self.populate_subnodes(self.subnodes_container)
 
     def populate_subnodes(self, container):
-        entries = sorted((e for e in os.scandir(self.folder_path) if e.is_dir() or e.is_file()), key=lambda e: e.name)
-        root_depth = len(self.folder_path.split(os.sep))
+        entries = sorted((e for e in os.scandir(self.path) if e.is_dir() or e.is_file()), key=lambda e: e.name)
+        root_depth = len(self.path.split(os.sep))
         for entry in entries:
             entry_depth = len(entry.path.split(os.sep))
             if entry_depth > root_depth + 1:
@@ -57,11 +104,13 @@ class FolderNode(Node):
     def toggle_subnodes(self):
         if self.expanded:
             self.hide_subnodes()
+            self.update_icon(self.collapsed_icon)
         else:
             self.show_subnodes()
+            self.update_icon(self.expanded_icon)
 
     def show_subnodes(self):
-        self.subnodes_container.pack(padx=(15, 0), pady=0, anchor='nw')
+        self.subnodes_container.pack(padx=(15, 0), pady=0, ipadx=0, ipady=0, anchor='nw')
         for subnode in self.subnodes:
             subnode.show()
         self.expanded = True
@@ -78,4 +127,57 @@ class FolderNode(Node):
 class TreeStructure(ctk.CTkFrame):
     def __init__(self, parent, root, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.root_node = FolderNode(self, root, level=0)
+        self.root_node = FolderNode(self, root)
+
+    # Public methods
+    def expand_all(self):
+        self._expand_all(self.root_node)
+
+    def collapse_all(self):
+        self._deselect_all(self.root_node)
+        self._collapse_all(self.root_node)
+
+    def select_all(self):
+        self._select_all(self.root_node)
+
+    def deselect_all(self):
+        self._deselect_all(self.root_node)
+
+    def get_selected_files(self):
+        selected_files = {}
+        self._get_selected_files(self.root_node, selected_files)
+        return selected_files
+
+    # Private methods
+    def _expand_all(self, node):
+        node.show_subnodes()
+        for subnode in node.subnodes:
+            if isinstance(subnode, FolderNode):
+                self._expand_all(subnode)
+
+    def _collapse_all(self, node):
+        node.hide_subnodes()
+        for subnode in node.subnodes:
+            if isinstance(subnode, FolderNode):
+                self._collapse_all(subnode)
+
+    def _select_all(self, node):
+        for subnode in node.subnodes:
+            if isinstance(subnode, FolderNode):
+                self._select_all(subnode)
+            elif isinstance(subnode, FileNode):
+                subnode.select()
+
+    def _deselect_all(self, node):
+        for subnode in node.subnodes:
+            if isinstance(subnode, FolderNode):
+                self._deselect_all(subnode)
+            elif isinstance(subnode, FileNode):
+                subnode.deselect()
+
+    def _get_selected_files(self, node, selected_files):
+        for subnode in node.subnodes:
+            if isinstance(subnode, FolderNode):
+                self._get_selected_files(subnode, selected_files)
+            elif isinstance(subnode, FileNode) and subnode.selected:
+                selected_files[subnode.name] = subnode.path
