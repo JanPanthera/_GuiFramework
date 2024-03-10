961fb9f7f73626f26e82fb98fd6fdfd450e5c7cc
Jan Görl <JanPanthera@outlook.de>
2024-03-10 22:40:33 +0100

    "Implemented Application and Window classes, updated __init__.py, and added constants.py"
    Updated the `__init__.py` file to import necessary modules and classes from `constants.py`, `window.py`, and `application.py`. Created a new `Application` class in `application.py` that inherits from `EventMixin` and includes methods for application lifecycle and main loop. Added a `constants.py` file to store constant values related to the framework. Created a new `Window` class in `window.py` that inherits from `LoggerMixin` and `EventMixin`, and includes methods for window management and event callbacks.

---
+++ GuiFramework/core/__init__.py
@@ -0,0 +1,6 @@
+# GuiFramework/core/__init__.py
+
+
+from .constants import *
+from .window import Window
+from .application import Application
---
+++ GuiFramework/core/constants.py
@@ -0,0 +1,11 @@
+# GuiFramework/core/constants.py
+
+
+FRAMEWORK_NAME = "GuiFramework"
+FRAMEWORK_VERSION = "0.1.0"
+FRAMEWORK_AUTHOR = "JanPanthera"
+FRAMEWORK_DESCRIPTION = "A simple GUI framework for Python using GLFW and OpenGL"
+FRAMEWORK_URL = ""
+FRAMEWORK_LICENSE = "MIT"
+FRAMEWORK_LICENSE_URL = "https://opensource.org/licenses/MIT"
+
