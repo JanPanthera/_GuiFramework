c1cedb8a673f8084433c0bb32f4ba3c7d53978ce
Jan Görl <JanPanthera@outlook.de>
2024-03-20 21:49:57 +0100

    `Update logger config and modify imports in __init__.py`
    Removed logger configuration in `__init__.py`, including the `Logger.add_logger` function call and its parameters. Modified the `__init__.py` file in the `GuiFramework/utilities/config/` directory, adjusting import statements and adding comments for future fixes. Updated the `__all__` list, repositioning and adding new elements.

--- GuiFramework/utilities/config/__init__.py
+++ GuiFramework/utilities/config/__init__.py
@@ -1,23 +1,26 @@
 # GuiFramework/utilities/config/__init__.py
 
 
-from .mixins import ConfigHandlerMixin
+from .mixins import ConfigHandlerMixin  # TODO: Fix to new changes
 from .mixins import ConfigFileHandlerMixin
 from .mixins import ConfigDynamicStoreMixin
 
-from .config_handler import ConfigHandler, ConfigVariable
+from .config_handler import ConfigHandler
 from .config_file_handler import ConfigFileHandler, ConfigFileHandlerConfig
 from .config_dynamic_store import ConfigDynamicStore
+from .config_types import ConfigKey, ConfigKeyList, ConfigVariable
 from .custom_type_handler_base import CustomTypeHandlerBase
 
 __all__ = [
-    "ConfigHandler",
+    "ConfigKey",
+    "ConfigKeyList",
     "ConfigVariable",
-    "ConfigHandlerMixin",
     "CustomTypeHandlerBase",
+    "ConfigHandler",
+    "ConfigHandlerMixin",
     "ConfigFileHandler",
     "ConfigFileHandlerMixin",
     "ConfigFileHandlerConfig",
     "ConfigDynamicStore",
-    "ConfigDynamicStoreMixin"
+    "ConfigDynamicStoreMixin",
 ]
