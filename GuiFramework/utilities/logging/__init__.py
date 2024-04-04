24f36bc4f801585781cd5a7bb6c1e8a203626375
Jan Görl <JanPanthera@outlook.de>
2024-04-04 20:28:59 +0200

    Updated file operations and logging configuration
    Updated several files to improve file operations and logging. In `executable_creator.py`, a print statement was added to the `add_data_file` method to print the flag being appended to the `additional_flags` list. The `sys` module was imported in `file_ops.py`. The `resolve_development_path` method in the `FileOps` class was significantly modified to check if the application is running as a standalone executable and to look for a directory named "GuiFramework" instead of a file named "main.py". A `FileNotFoundError` is now raised if the "GuiFramework" directory cannot be found. In `__init__.py`, the `log_directory` argument in the `LoggerConfig` instantiation was updated to use "GuiFramework" as the `root_marker` in the `resolve_development_path` method.

--- GuiFramework/utilities/logging/__init__.py
+++ GuiFramework/utilities/logging/__init__.py
@@ -15,7 +15,7 @@ Logger.add_logger(
     LoggerConfig(
         logger_name="GuiFramework",
         log_name="gui_framework",
-        log_directory=fops.resolve_development_path(__file__, "logs", ".root"),
+        log_directory=fops.resolve_development_path(__file__, "logs", "GuiFramework"),
         log_level=LOG_LEVEL.DEBUG,  # Change this to LOG_LEVEL.INFO for production
         module_name="GuiFramework"
     ),
